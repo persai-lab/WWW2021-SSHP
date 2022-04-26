f57d01d1fcaf22d165f225337883bdec19fae728
subhendukhatuya <subha.cse143@gmail.com>
2022-04-26 16:52:56 +0530

    Remove redundant print runner

--- runner.py
+++ runner.py
@@ -16,7 +16,6 @@ for i in range(M):
             M0.append(1200)
 M0 = np.array(M0).reshape(M,N)
 
-print('M0', M0)
 init = {}
 init['A0'] = abs(np.random.normal(loc = 1,scale = 0.1,size = (M,N)))
 init['M0'] = M0
@@ -31,3 +30,4 @@ init['Gamma_h0'] = np.ones((M,N))*10
 decay = 10 #hyperparameter
 est,funcVal = customized_Hawkes(data, init, mask, decay, 0.5, 25, term_cond =1)
 scipy.io.savemat(f'est_decay_{decay}', mdict=est) # save estimated params
+est
